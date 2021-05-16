d11f3ed6271d4fe5d97f20ea8838dbd720ddb406
Kevin Lewis <kvn@lws.io>
2021-05-16 22:58:05 +0100

    Redirects

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,10 +6,12 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'I am a Developer Advocate & Event Organizer' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' },
+      { rel: 'shortcut icon', href: '/favicons/favicon.ico' }
     ]
   },
   components: true,
